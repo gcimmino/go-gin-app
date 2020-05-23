acc291fe9fdbb2551537a373bf109488ff73b864
Giovanni Cimmino <cimminogi@gmail.com>
2020-05-23 19:09:47 +0200

    add tests

--- handlers.article_test.go
+++ handlers.article_test.go
@@ -24,3 +24,43 @@ func TestShowIndexPageUnauthenticated(test *testing.T) {
     return statusOK && pageOK
   })
 }
+
+func TestShowIndexPageAuthenticated(test *testing.T)  {
+  w := httptest.NewRecorder()
+
+  router := getRouter(true)
+
+  http.SetCookie(w, &http.Cookie{Name: "token", Value: "123"})
+
+  router.GET("/", showIndexPage)
+
+  request, _ := http.NewRequest("GET", "/", nil)
+  request.Header = http.Header{"Cookie": w.HeaderMap["Set-Cookie"]}
+
+  router.ServeHTTP(w, request)
+
+  if w.Code != http.StatusOK {
+    test.Fail()
+  }
+
+  page, err := ioutil.ReadAll(w.Body)
+
+  if err != nil || strings.Index(string(page), "<title>Home Page</title>") < 0 {
+    test.Fail()
+  }
+}
+
+func TestArticleUnauthenticated(test *testing.T)  {
+  router := getRouter(true)
+  router.GET("/article/view/:article_id", getArticle)
+  request, _ := http.NewRequest("GET", "/article/view/1", nil)
+
+  testHTTPResponse(test, router, request, func(w *httptest.ResponseRecorder) bool {
+    statusOK := w.Code == http.StatusOK
+
+    page, err := ioutil.ReadAll(w.Body)
+    pageOK := err == nil && strings.Index(string(page), "<title>Article 1</title>") > 0
+
+    return statusOK && pageOK
+  })
+}
