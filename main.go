d6749b3eeb292a1b27ac0c5286933583d94e1568
Giovanni Cimmino <cimminogi@gmail.com>
2020-05-23 14:04:20 +0200

    remove commented code

--- main.go
+++ main.go
@@ -2,29 +2,12 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-// import "routes"
-
-// import (
-//   "net/http"
-//   "github.com/gin-gonic/gin"
-// )
-
 var router *gin.Engine
 
 func main() {
   router = gin.Default()
 
   router.LoadHTMLGlob("templates/*")
-
-  // router.GET("/", func(context *gin.Context)  {
-  //   context.HTML(
-  //     http.StatusOK,
-  //     "index.html",
-  //     gin.H{
-  //       "title": "Home Page",
-  //     },
-  //   )
-  // })
 
   initializeRoutes()
 
